c1705e12052e2683dd998274b2e8f3c75ba3fb33
viz <vizbone@users.noreply.github.com>
2019-07-08 15:01:35 +0800

    grenade in temp script added

---
+++ NeoDymium/Assets/Scripts/temp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class temp : MonoBehaviour
+{
+	public Transform grenadePos;
+    public float throwSpeed;
+	public GameObject grenade;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (key: KeyCode.G)) 
+		{
+			Rigidbody grenadeRb = Instantiate (grenade, grenadePos.position, Quaternion.identity).GetComponent<Rigidbody> ();
+			//grenadeRb.velocity = [player.transform.forward] + new Vector3 (0, [player.transform.forward] * (throwSpeed - 1), 0);
+		}
+	}
+}

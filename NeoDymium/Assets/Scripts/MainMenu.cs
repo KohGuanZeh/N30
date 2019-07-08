a83b0371444edb2a39a3b7f4edfdd8e53a39fbf4
Koh Guan Zeh <kohguanzeh960@gmail.com>
2019-07-08 14:40:12 +0800

    Additional Files

---
+++ NeoDymium/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public GameObject menuObj;
+	public GameObject creditsObj;
+
+	void Start () 
+	{
+		menuObj.SetActive (true);
+		creditsObj.SetActive (false);
+	}
+
+    public void Play () 
+	{
+		SceneManager.LoadScene ("playground");
+	}
+
+	public void Credits () 
+	{
+		menuObj.SetActive (!menuObj.activeSelf);
+		creditsObj.SetActive (!creditsObj.activeSelf);
+	}
+
+	public void Quit () 
+	{
+		Application.Quit ();
+	}
+}
--- NeoDymium/Assets/Scripts/UIManager.cs
+++ NeoDymium/Assets/Scripts/UIManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+	public GameObject mainObj;
+	public GameObject pauseObj;
+
     public TextMeshProUGUI healthText;
 
 	PlayerShooterController player;
@@ -10,10 +14,28 @@ public class UIManager : MonoBehaviour
 	void Start () 
 	{
 		player = PlayerShooterController.inst;
+		mainObj.SetActive (true);
+		pauseObj.SetActive (false);
 	}
 	
 	void Update () 
 	{
+		if (Input.GetKeyDown (key: KeyCode.Escape)) 
+			PausePlay ();
+
 		healthText.text = player.currentHealth + "/" + player.maxHealth;
+	}
+
+	public void PausePlay () 
+	{
+		mainObj.SetActive (!mainObj.activeSelf);
+		pauseObj.SetActive (!pauseObj.activeSelf);
+		Cursor.lockState = mainObj.activeSelf ? CursorLockMode.Locked : CursorLockMode.None;
+		Time.timeScale = mainObj.activeSelf ? 1 : 0;
+	}
+
+	public void MainMenu () 
+	{
+		SceneManager.LoadScene ("Main Menu");
 	}
 }

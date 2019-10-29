2ea0c2891957e562859220d6f6694bf037c3373d
Nigel Koh <kohyuanqi@gmail.com>
2019-10-29 21:08:07 +0800

    Tutorial Update, Door Update

--- NeoDymium/Assets/Scripts/AIDoor.cs
+++ NeoDymium/Assets/Scripts/AIDoor.cs
@@ -5,25 +5,30 @@ public class AIDoor : MonoBehaviour
 	public ColorIdentifier requiredColor;
 	
 	Animator animator;
-	Collider col;
 
 	void Start ()
 	{
 		animator = GetComponent<Animator> ();
-		col = GetComponent<Collider> ();
+	}
+
+	void Open ()
+	{
+		SoundManager.inst.PlaySound (SoundManager.inst.slidingDoor);
+		RespectiveGoals goal = GetComponent<RespectiveGoals>();
+		if (goal) goal.isCompleted = true;
+		animator.SetBool  ("Opened", true);
 	}
 
 	void OnTriggerStay (Collider other)
 	{
 		if ((other.tag == "Hackable" && other.GetComponent<IHackable> ().color == requiredColor))
 		{
-			//transform.GetChild (0).gameObject.SetActive (false);
-			SoundManager.inst.PlaySound (SoundManager.inst.slidingDoor);
-			RespectiveGoals goal = GetComponent<RespectiveGoals>();
-			if (goal) goal.isCompleted = true;
-			//gameObject.SetActive (false);
-			animator.SetBool  ("Opened", true);
-			col.enabled = false;
+			Open ();
+		}
+
+		if (other.tag == "Player" && requiredColor == ColorIdentifier.none)
+		{
+			Open ();
 		}
 	}
 }

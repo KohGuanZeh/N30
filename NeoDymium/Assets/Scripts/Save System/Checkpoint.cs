a388cbab081f3bf816ef7a4d8cda6ea0114b512d
Koh Guan Zeh <kohguanzeh960@gmail.com>
2019-12-02 12:13:38 +0800

    Edited Tutorial 2 and Updated Checkpoint System

--- NeoDymium/Assets/Scripts/Save System/Checkpoint.cs
+++ NeoDymium/Assets/Scripts/Save System/Checkpoint.cs
@@ -8,7 +8,6 @@ public class Checkpoint : MonoBehaviour
 	//May have a System whereby upon Player/UI Manager keeps track of which Checkpoint to spawn, Loop through the list of the Checkpoints up to the Player Spawn and call their Load Function
 	//to Disable the Control Panels that are meant to be disabled
 	[SerializeField] PlayerController player;
-	[SerializeField] LayerMask playerLayer;
 	[SerializeField] Collider coll;
 	// [SerializeField] GameObject wall;
 	[SerializeField] List<IHackable> hackables;
@@ -24,8 +23,16 @@ public class Checkpoint : MonoBehaviour
 	
 	void Start ()
 	{
-
 		uIManager = UIManager.inst;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			PlayerPrefs.DeleteKey("Last Objective Saved");
+			LoadingScreen.inst.LoadScene(SceneManager.GetActiveScene().name);
+		} 
 	}
 
 	public void LoadCheckPoint()
@@ -41,10 +48,13 @@ public class Checkpoint : MonoBehaviour
 	private void OnTriggerEnter(Collider other)
 	{
 		//If detected Player, Considered Check point Registered
-		if (playerLayer == (playerLayer | (1 << other.gameObject.layer)))
+		if (other.gameObject.tag == "Player")
 		{
 			uIManager.touchedCheckpoint = true;
 			uIManager.ShowSavedAfterCheckpoints();
+			player.checkPointsPassed++;
+			PlayerPrefs.SetInt("Scene Index", SceneManager.GetActiveScene().buildIndex);
+			PlayerPrefs.SetInt("Last Objective Saved", player.checkPointsPassed);
 			// if (wall) wall.SetActive(true);
 			coll.enabled = false;
 		}

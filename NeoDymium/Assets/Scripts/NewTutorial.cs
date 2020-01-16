b59a48f9d0086e71724c17ba2336a8f05cb6fee3
viz <vizbone@users.noreply.github.com>
2020-01-16 18:09:14 +0800

    wip tutorial instruction

---
+++ NeoDymium/Assets/Scripts/NewTutorial.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public struct TutorialSegment
+{
+	public bool triggered; //put to false at start
+	[TextArea (5, 20)]
+	public string description;
+}
+
+public class NewTutorial : MonoBehaviour
+{
+	public TutorialSegment[] tutorials;
+	[Space (10)]
+	public TextMeshProUGUI descriptionText;
+	public GameObject tutorialObj;
+
+	int currentTutorialIndex = -1;
+
+	void Start ()
+	{
+		tutorialObj.SetActive (false);
+	}
+
+	public void TutorialStart (int index)
+	{
+		if (!tutorials[index].triggered)
+		{
+			currentTutorialIndex = index;
+			tutorials[index].triggered = true;
+			descriptionText.text = tutorials[index].description;
+			tutorialObj.SetActive (true);
+		}
+	}
+
+	public void TutorialEnd (int index)
+	{
+		if (index == currentTutorialIndex)
+			tutorialObj.SetActive (false);
+	}
+}
---
+++ NeoDymium/Assets/Scripts/TriggerUnityEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Tutorial : MonoBehaviour
+{
+	public UnityEvent events;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player" || other.tag == "HackableInteractable")
+		{
+			events.Invoke ();
+		}
+	}
+}

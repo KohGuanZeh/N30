0c89cc7c5b11cb61fbda40c9f8c337535de29ef0
Nigel Koh <kohyuanqi@gmail.com>
2020-02-06 17:09:38 +0800

    Bug Fixes from Clarence mini Critique, Added Exit Signs, Fixed Level 3 Lighting

---
+++ NeoDymium/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	public float rotateSpeed;
+	public bool clockwise;
+
+	void Update ()
+	{
+		transform.eulerAngles += Vector3.forward * rotateSpeed * Time.deltaTime * (clockwise ? 1 : -1);
+	}
+}

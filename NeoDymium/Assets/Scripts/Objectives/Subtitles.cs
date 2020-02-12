5b542735ae5d20b7050cfd3d8f1b519058ea6b01
Nigel Koh <kohyuanqi@gmail.com>
2020-02-12 17:37:23 +0800

    Add Subtitles, Fixed small bugs

---
+++ NeoDymium/Assets/Scripts/Objectives/Subtitles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subtitles : MonoBehaviour
+{
+    public string[] subtitlesList;
+    UIManager uIManager;
+
+    void Start ()
+    {
+        uIManager = UIManager.inst;
+    }
+
+    void OnTriggerEnter()
+    {
+        StartCoroutine (uIManager.ShowHideSubtitles(subtitlesList));
+    }
+}
